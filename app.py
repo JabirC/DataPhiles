8b9f23bd88110e34024490e91ffe49b50da2b457
Nikola Terran <nikolaterranthe1@gmail.com>
2019-03-29 10:36:45 -0400

    s

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 #Team DataPhiles
 
+"""
+dataset = lottery
+"""
+
 from flask import *
+import os
+import json
+from pymongo import MongoClient
 
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
-@route("/"):
-def home()
-	
+@app.route('/', methods=['GET','POST'])
+def home():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()	
