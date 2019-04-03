81f850287bf0c82c2d2c7bb5e7fa79c23d15284d
Brian Lee <hlee5@stuy.edu>
2019-04-02 21:57:42 -0400

    aaaa

--- static/racingbar.js
+++ static/racingbar.js
@@ -1,198 +1,61 @@
-
-var data_global;
-
 var chart = function(dataset) {
   var startYear = 2002;
   var height = 600;
 
+  var nTop = 75;
+  var width = 1000;
+  var height = 500;
+
   const svg = d3.select('svg');
-  //console.log(svg);
-  data_global = dataset;
 
   const margin = {
     top: 80,
-    right: 0,
-    bottom: 5,
-    left: 0
+    right: 10,
+    bottom: 10,
+    left: 30
   };
   
   let title = svg.append('text')
     .attr('class', 'title')
 	.attr('y', '24')
-    .text('insert title here');
-
-  let year = startYear;
-  var month_counter = 1;
-  var month_txt = 5;
-  var year_txt = 2002;
-  console.log(dataset);
+    .text('Lottery Numbers');
   
-  //get_month(dataset[0][0]);
-  svg.append("text")
+  var xScale = d3.scaleLinear()
+                 .domain([0, 200])
+                 .range([margin.left, width - margin.right]);
+  var yScale = d3.scaleLinear()
+                 .domain([0, nTop])
+                 .range([margin.top, height - margin.bottom]);
+  var xAxis = d3.axisTop().scale(xScale);
+  var yAxis = d3.axisLeft().ticks(0).scale(yScale);
+  svg.append("g")
+      .attr("transform", "translate(0," + margin.top + ")")
+      .call(xAxis);
+  svg.append("g")
+      .attr("transform", "translate(" + margin.left + ",0)")
+      .call(yAxis);
 
-  svg.append("text")
-      .text("month: " + month_txt)
-      .attr('class','title')
-      .attr('y', '500')
-      .attr('x', '150')
-      .attr('id','month')
-      
-  svg.append("text")
-    .text("year: " + year_txt)
-    .attr('class','title')
-    .attr('y', '500')
-    .attr('x', '50')
-    .attr('id','year')
-
-  if(get_month(dataset[dataset.length - month_counter][0]) == month_txt){
-     
+  // Get list of all lottery numbers
+  var lottery_list = dataset.map(x => x[1]).flat().map(x => +x).sort(function(a, b) {return a-b;});
+  var counts = {};
+  for (var i = 0; i < lottery_list.length; i++) {
+    var num = lottery_list[i];
+    counts[num] = counts[num] ? counts[num] + 1: 1;
   }
 
-  //while(month_txt < 500){
-    then = Date.now();
-    animation();
-  //  month_txt++;
-  //  console.log(month_txt);
-  //}
-  /*
-  while(dataset[dataset.length - month_counter]){
-    console.log(dataset[dataset.length - month_counter][0]);
-    month_counter++;
-  }
-  */
-
-  //code below is copied from the demo and most of it should be changed tbh
-  // dataset.forEach(d => {
-  //   d.value = +d.value,
-  //   d.lastValue = +d.lastValue,
-  //   d.value = isNaN(d.value) ? 0 : d.value,
-  //   d.year = +d.year,
-  //   // d.colour = d3.hsl(Math.random()*360,0.75,0.75)
-  //   d.colour = "#C8BDFF"
-  // });
-  
-  // let yearSlice = dataset.filter(d => d.year == year && !isNaN(d.value))
-  //   .sort((a,b) => b.value - a.value)
-  //   .slice(0,top_n);
-  
-  // yearSlice.forEach((d,i) => d.rank = i);
-  
-  // let x = d3.scaleLinear()
-  //   .domain([0, d3.max(yearSlice, d => d.value)])
-  //   .range([margin.left, width-margin.right-65]);
-  
-  // let y = d3.scaleLinear()
-  //   .domain([top_n, 0])
-  //   .range([height-margin.bottom, margin.top]);
-  
-  // let colourScale = d3.scaleOrdinal()
-  //   .range(["#adb0ff", "#ffb3ff", "#90d595", "#e48381", "#aafbff", "#f7bb5f", "#eafb50"])
-  //   .domain(["India","Europe","Asia","Latin America","Middle East","North America","Africa"]);
-  //   // .domain(groups);
-  
-  // let xAxis = d3.axisTop()
-  //   .scale(x)
-  //   .ticks(width > 500 ? 5:2)
-  //   .tickSize(-(height-margin.top-margin.bottom))
-  //   .tickFormat(d => d3.format(',')(d));
-  
-  // svg.append('g')
-  //   .attrs({
-  //     class: 'axis xAxis',
-  //     transform: `translate(0, ${margin.top})`
-  //   })
-  //   .call(xAxis)
-  //     .selectAll('.tick line')
-  //     .classed('origin', d => d == 0);
-  
-  // svg.selectAll('rect.bar')
-  //   .data(yearSlice, d => d.name)
-  //   .enter()
-  //   .append('rect')
-  //   .attrs({
-  //     class: 'bar',
-  //     x: x(0)+1,
-  //     width: d => x(d.value)-x(0)-1,
-  //     y: d => y(d.rank)+5,
-  //     height: y(1)-y(0)-barPadding
-  //   })
-  //   .styles({
-  //     fill: d => colourScale(d.group)
-  //     // fill: d => d.colour
-  //   });
-  
-  // svg.selectAll('text.label')
-  //   .data(yearSlice, d => d.name)
-  //   .enter()
-  //   .append('text')
-  //   .attrs({
-  //     class: 'label',
-  //     transform: d => `translate(${x(d.value)-5}, ${y(d.rank)+5+((y(1)-y(0))/2)-8})`,
-  //     'text-anchor': 'end'
-  //   })
-  //   .selectAll('tspan')
-  //   .data(d => [{text: d.name, opacity: 1, weight:600}, {text: d.subGroup, opacity: 1, weight:400}])
-  //   .enter()
-  //   .append('tspan')
-  //   .attrs({
-  //     x: 0,
-  //     dy: (d,i) => i*16
-  //   })
-  //   .styles({
-  //     // opacity: d => d.opacity,
-  //     fill: d => d.weight == 400 ? '#444444':'',
-  //     'font-weight': d => d.weight,
-  //     'font-size': d => d.weight == 400 ? '12px':''
-  //   })
-  //   .html(d => d.text);
-  
-  // svg.selectAll('text.valueLabel')
-  //   .data(yearSlice, d => d.name)
-  //   .enter()
-  //   .append('text')
-  //   .attrs({
-  //     class: 'valueLabel',
-  //     x: d => x(d.value)+5,
-  //     y: d => y(d.rank)+5+((y(1)-y(0))/2)+1,
-  //   })
-  //   .text(d => d3.format(',')(d.lastValue));
-  
+ // this is broken
+  // var x = d3.scaleLinear().domain([0, d3.max(d3.values(counts))]).range([0,width]);
+  // var y = d3.scaleLinear().domain([0, d3.max(d3.keys(counts))]).range([0,50]);
+  // svg.selectAll("g")
+     // .data(d3.values(counts))
+     // .enter()
+     // .append("rect")
+         // .attr("x", margin.left)
+         // .attr("y", y)
+         // .attr("width", x)
+         // .attr("height", 5);
 }
 
-var id;
-var month = 5;
-var year = 2002;
-
-var fps, fpsInterval, startTime, now, then, elapsed;
-fpsInterval = 700;
-
-var animation = function(){
-  cancelAnimationFrame(id);
-
-  now = Date.now();
-  elapsed = now - then;
-  console.log(elapsed);
-  if (elapsed > fpsInterval) {
-          const svg = d3.select('svg');
-          //console.log("hello");
-          if(month > 12){
-              month = 1;
-              year++;
-            svg.selectAll('#year')
-             .text("year: " + year)
-          }
-
-          svg.selectAll('#month')
-             .text("month: " + month)
-          
-          console.log(data_global[0][0]);
-
-          month++;
-      then = now - (elapsed % fpsInterval);
-  }
-
-  id = requestAnimationFrame(animation);
-}
 
 var get_year = function(year){
   year = year.substring(0,3);
