8690cbde6d193908f1a8d7d9f064d80e5163c33d
Brian Lee <hlee5@stuy.edu>
2019-04-04 19:44:47 -0400

    map update

--- static/map.js
+++ static/map.js
@@ -1,6 +1,6 @@
 var chart = function(data) {
     var margin = {top : 0, left: 0, right: 0, bottom: 0},
-        height = 800 - margin.top - margin.bottom,
+        height = 600 - margin.top - margin.bottom,
         width = 1200 - margin.left - margin.right;
 
     var svg = d3.select("#map")
@@ -10,29 +10,48 @@ var chart = function(data) {
                 .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Function for projecting longitude/latitude into a map
-    var projection = d3.geoMercator()
-                       .translate([width / 2, height / 2])
-
     // Function for mapping the magnitude of an earthquake to radius of the dot
-    var r  = d3.scaleLinear()
-               .domain([2, 12])
-               .range([1, 10]);
+    var r = d3.scaleLinear()
+              .domain([0, 12])
+              .range([0, 10]);
 
-    svg.selectAll("circle")
-       .data(data)
-       .enter()
-       .append("circle")
-           .attr("cx", function(d) {
-               let point = [d["LONGITUDE"], d["LATITUDE"]];
-               return projection(point)[0];
-           })
-           .attr("cy", function(d) {
-               let point = [d["LONGITUDE"], d["LATITUDE"]];
-               return projection(point)[1];
-           })
-           .attr("r", function(d) {
-               return r(d["EQ_PRIMARY"]);
-           })
-           .attr("fill", "black");
+    var worldmap = d3.json("static/geodata.json").then(function (geojson) { 
+
+        // Function for projecting longitude/latitude into a flat map
+        let projection = d3.geoEquirectangular()
+                           .fitSize([width, height], geojson);
+
+        let path = d3.geoPath()
+                     .projection(projection);
+
+        // Draw the world map
+        svg.selectAll("path")
+            .data(geojson.geometries)
+            .enter()
+            .append("path")
+                .attr("d", path)
+                .style("fill", "#cccccc")
+                .style("stroke", "#888888");
+
+        // Plot earthquakes
+        svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("cx", function(d) {
+                    let point = [d["LONGITUDE"], d["LATITUDE"]];
+                    return projection(point)[0];
+                })
+                .attr("cy", function(d) {
+                    let point = [d["LONGITUDE"], d["LATITUDE"]];
+                    return projection(point)[1];
+                })
+                .style("fill", "rgb(250, 50, 50)")
+                .style("stroke", "#555555")
+                .transition()
+                .duration(2000)
+                    .attr("r", function(d) {
+                        return r(d["EQ_PRIMARY"]);
+                    });
+    });
 };
